67eed364cd84b42eda53a4834a4db78bf50b3ef2
Ph0-ever <oluwayomiforever@gmail.com>
2022-06-29 21:30:42 +0100

    feat: added a little feat

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+// document.getElementsByClassName('myswiper');
+// var swiper = new swiper(".myswiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+
 document.getElementsByClassName('myswiper');
-var swiper = new Swiper(".myswiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+var swiper = new swiper(".myswiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
     pagination: {
         el: ".swiper-pagination",
-        clickable: true,
+        clickable: true
     },
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+        prevEl: ".swiper-button-prev"
+    }
 });
+
+
